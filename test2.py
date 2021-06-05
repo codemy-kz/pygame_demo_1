97b39641a4dd09253ab205f9b79b613aab9b6fcf
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-05 19:51:33 +0600

    Графикалық примитивтерді сызу

--- test2.py
+++ test2.py
@@ -18,10 +18,19 @@ FPS = 60
 clock = pygame.time.Clock()
 
 screen.fill(WHITE)
-pygame.display.flip() # буффер
 
-rect = pygame.draw.rect(screen, BLUE, (50, 200, 50, 100))
-pygame.display.update(rect) # тек төртбұрышты жаңартады
+pygame.draw.rect(screen, BLUE, (10, 100, 50, 100), 3)
+pygame.draw.line(screen, GREEN, (200, 20), (350, 50), 3)
+pygame.draw.aaline(screen, GREEN, (200, 40), (350, 70), 3)
+pygame.draw.lines(screen, RED, True, [(200, 80), (250, 80), (300, 200)], 3)
+pygame.draw.aalines(screen, RED, True, [(300, 80), (350, 80), (400, 200)], 3)
+pygame.draw.polygon(screen, BLACK, [(150, 210), (180, 250), (90, 290), (30, 230)], 3)
+pygame.draw.circle(screen, BLUE, (650, 100), 40, 2)
+pygame.draw.ellipse(screen, BLUE, (600, 250, 150, 50), 3)
+pi = 3.14
+pygame.draw.arc(screen, RED, (500, 300, 150, 50), pi/2, 2*pi, 3)
+
+pygame.display.update() 
 
 flRunning = True
 
