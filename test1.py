2c15497e8bffc0c61213d384b8d3eb16e7d775dd
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-05 18:34:50 +0600

    Программаның каркасы толықтырылды

--- test1.py
+++ test1.py
@@ -1,15 +1,24 @@
 import pygame
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 pygame.init()
 
-screen_width = 600
-screen_height = 600
+screen_width = 800
+screen_height = int(600 * 0.8)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("PyGame Test App")
 
 FPS = 60
 clock = pygame.time.Clock()
+
+screen.fill(WHITE)
+pygame.display.update()
 
 flRunning = True
 
