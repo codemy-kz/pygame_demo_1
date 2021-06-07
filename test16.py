04953c14319c3cede9e63a14c9a0f9fd71d46d01
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-07 11:21:34 +0600

    Спрайт толығымен қолданылды

--- test16.py
+++ test16.py
@@ -1,10 +1,12 @@
 import pygame
 from figure import Figure
+import random
 
 SCREEN_WIDTH = 1380
 SCREEN_HEIGHT = 580
 
 pygame.init()
+pygame.time.set_timer(pygame.USEREVENT, 2000)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame Sprite Demo App")
 
@@ -12,9 +14,16 @@ bg_surf = pygame.image.load("images/back1.jpg").convert()
 
 figures = pygame.sprite.Group()
 
-figures.add(Figure("images/ball_bear.png",  SCREEN_WIDTH//2, 1))
-figures.add(Figure("images/ball_fox.png",  SCREEN_WIDTH//2 - 250, 3),
-    Figure("images/ball_panda.png",  SCREEN_WIDTH//2 + 150, 2))
+fig_images = ['ball_bear.png', 'ball_fox.png', 'ball_panda.png', 'ball_lemur.png',
+            'ball_tiger.png', 'ball_puppy.png', 'ball_kitten.png']
+fig_surf = [pygame.image.load('images/' + image) for image in fig_images]
+
+def create_figure(group):
+    index = random.randint(0, len(fig_surf)-1)
+    figure = fig_surf[index]
+    speed = random.randint(1, 4)
+    from_x = random.randint(30, SCREEN_WIDTH-30)
+    return Figure(figure, from_x, speed, group)
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -25,6 +34,8 @@ while flRunning:
         if event.type == pygame.QUIT:
             flRunning = False
             pygame.quit()
+        elif event.type == pygame.USEREVENT:
+            create_figure(figures)
 
     
     screen.blit(bg_surf, (0,0))
